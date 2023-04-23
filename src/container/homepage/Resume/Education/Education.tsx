838cb1028a6a8ccc5a02350d81991a19e16e03e2
Satyam Nikhra <neekhrasatyam1993@gmail.com>
2023-04-23 16:25:44 -0700

    fix Education.tsx

--- src/container/homepage/Resume/Education/Education.tsx
+++ src/container/homepage/Resume/Education/Education.tsx
@@ -13,7 +13,7 @@ const Education = () => {
           Dr. A. P. J. Abdul Kalam Technical University, India
         </div>
         <div className="italic mobile:flex-col mt-[8px] mobile:mt-[9px]  text-[#6E7881] mobile:mb-[18px] mobile:gap-0 flex gap-[2px]">
-          Bachelor's in Electrical & Electronics Engineering,GPA 3.77/4.0
+          Bachelor&apos;s in Electrical & Electronics Engineering,GPA 3.77/4.0
           <div className="pt-[4px] mobile:hidden">
             <GoPrimitiveDot />
           </div>
